4e6912175ad5154c98e9c5cfc6463ad8108c1eb1
Renan Almeida <renanalmeida2.ra@gmail.com>
2023-01-01 23:47:47 -0300

    a

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
+import { Text } from 'react-native'
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Text></Text>
     </Container>
   );
 }
