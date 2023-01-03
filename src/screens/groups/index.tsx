8fd83834cd68b15c25d75d31ed05f40b684d3bce
Renan Almeida <101940943+RenanMaestrya@users.noreply.github.com>
2023-01-03 10:07:34 -0300

    Feat: Components GroupCard n ListEmpty

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,14 +1,36 @@
+import { useState } from 'react';
+
+import { FlatList } from 'react-native'
+
 import { Header } from '@components/Header';
-import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+
+import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera do Pechinchas', 'ai papai'])
+
   return (
     <Container>
       <Header />
       <Highlight 
         title='Turmas' 
         subtitle='Jogue com a sua turma'  
+      />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        ListEmptyComponent= {() => <ListEmpty 
+          message='Que tal cadastrar a primeira turma?' 
+        />}
       />
     </Container>
   );
